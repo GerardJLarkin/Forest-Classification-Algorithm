b0ad4294101bb9719713c3ae957b7e8688e1222d
Dylan Moore <hosnappp@gmail.com>
2016-03-05 18:14:02 -0500

    Changed formatting
    user interface change

--- decisiontree.py
+++ decisiontree.py
@@ -2,9 +2,12 @@ import pandas as pd
 import time
 
 name = input("What is the filename? \n")
+print('\nReading from '+name)
+time.sleep(1)
 header = pd.read_csv(name)
 numcol = len(header.columns)
-print('\n'+str(numcol)+' nodes.'+'\n')
+print('\n[+] '+str(numcol)+' nodes.')
+time.sleep(1)
 col = pd.read_csv(name, usecols=[0])
-print('\n[+] Running entropy analysis on each node...')
+print('\n[~] Running entropy analysis on each node...')
 time.sleep(1)
