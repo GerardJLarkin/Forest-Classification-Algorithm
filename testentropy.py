07a7731eb6ddca2790f183fa4fa62d24842ff110
Dylan Moore <hosnappp@gmail.com>
2016-03-06 18:29:05 -0500

    Entropy module done
    v1.0 of entropy module completed. If there's a nicer way to do the math,
    let me know someone.

---
+++ testentropy.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  6 18:13:28 2016
+
+@author: Dylan
+"""
+
+import math
+
+yes = int(input('What is the number of yes?: \n'))
+no = int(input('what is the number of no?: \n'))
+
+entropy = -(yes/(yes+no))*math.log1p(2)*(yes/(yes+no))*-(no/(yes+no))*math.log1p(2)*(no/(yes+no))
+
+print('[+] Entropy on sample node: '+str(math.fabs(entropy)))10
