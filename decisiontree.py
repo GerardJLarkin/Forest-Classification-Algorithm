73bfd55c00fb8661db5f555f9aa801c3fb9080d3
Dylan Moore <hosnappp@gmail.com>
2016-03-06 20:00:15 -0500

    Couple bad changes, working still
    Yup

--- decisiontree.py
+++ decisiontree.py
@@ -1,13 +1,29 @@
 import pandas as pd
 import time
+from operator import add
 
 name = input("What is the filename? \n")
 print('\nReading from '+name)
 time.sleep(1)
-header = pd.read_csv(name)
-numcol = len(header.columns)
-print('\n[+] '+str(numcol)+' nodes.')
+csv = pd.read_csv(name)
+df = pd.DataFrame(data=csv)
+numcol = len(csv.columns)
+print('\n[+] '+str(numcol)+' classifications found.')
 time.sleep(1)
-col = pd.read_csv(name, usecols=[0])
+row1 = pd.read_csv(name, usecols=[0])
 print('\n[~] Running entropy analysis on each node...')
 time.sleep(1)
+print(df.columns)
+col00 = str(df.iloc[0,0])
+i = [1,0] # initialize column position
+r = [1]
+work = range(len(row1))
+for each in work:
+    if str(df.iloc[i]) == col00:
+        map(add, i, r)
+        remain = len(row1) - 1
+    elif str(df.iloc[i]) != col00:
+        map(add,i,r)
+        col10=str(df.iloc[1,0])
+        remain = len(row1) - 1
+        print('\n [+] Found child node... '+str(remain)+' checks left.')
--- testentropy.py
+++ testentropy.py
@@ -10,6 +10,6 @@ import math
 yes = int(input('What is the number of yes?: \n'))
 no = int(input('what is the number of no?: \n'))
 
-entropy = -(yes/(yes+no))*math.log1p(2)*(yes/(yes+no))*-(no/(yes+no))*math.log1p(2)*(no/(yes+no)) //http://i.imgur.com/xVNo9ZE.png
+entropy = -(yes/(yes+no))*math.log1p(2)*(yes/(yes+no))*-(no/(yes+no))*math.log1p(2)*(no/(yes+no)) # http://i.imgur.com/xVNo9ZE.png
 
 print('[+] Entropy on sample node: '+str(math.fabs(entropy)))10
