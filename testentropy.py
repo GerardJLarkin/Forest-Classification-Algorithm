ab721ff826a3eb9888e5c55845b0ce9241bdccd5
Dylan Moore <hosnappp@gmail.com>
2016-03-06 21:05:42 -0500

    Typo
    fixed

--- testentropy.py
+++ testentropy.py
@@ -12,4 +12,4 @@ no = int(input('what is the number of no?: \n'))
 
 entropy = -(yes/(yes+no))*math.log1p(2)*(yes/(yes+no))*-(no/(yes+no))*math.log1p(2)*(no/(yes+no)) # http://i.imgur.com/xVNo9ZE.png
 
-print('[+] Entropy on sample node: '+str(math.fabs(entropy)))10
+print('[+] Entropy on sample node: '+str(math.fabs(entropy)))
